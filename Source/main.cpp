f4e72aa469e68f7a279495ff603e7d864735d8bc
amSangi <sangiamir95@gmail.com>
2018-12-14 00:31:57 -0800

    Setup Qt5 with cmake

--- Source/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    return 0; // stub
-}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <QApplication>
+#include "ZeroMath.h"
+
+int main(int argc, char *argv[])
+{
+	QApplication app( argc, argv );
+	return app.exec();
+}
