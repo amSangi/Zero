bc3c6b6152e7f4ec4644adbe36cc85260b49d4e4
amSangi <sangiamir95@gmail.com>
2020-11-04 21:15:06 -0800

    [RenderSystem] Fixed OrthographicViewVolumeTests

--- test/src/render/OrthographicViewVolumeTests.cpp
+++ test/src/render/OrthographicViewVolumeTests.cpp
@@ -13,10 +13,9 @@ protected:
     TestOrthographicViewVolume()
     : camera_(Camera::ProjectionType::ORTHOGRAPHIC)
     , volume_(nullptr)
-    , near_bottom_left_()
-    , near_top_right_()
-    , far_bottom_left_()
-    , far_top_right_()
+    , min_()
+    , max_()
+    , difference_()
     {
     }
 
@@ -24,32 +23,31 @@ protected:
     {
         std::shared_ptr<IViewVolume> view_volume = ViewVolumeBuilder::create(camera_);
         volume_ = std::static_pointer_cast<OrthographicViewVolume>(view_volume);
-        camera_.GetNearClipCoordinates(near_bottom_left_, near_top_right_);
-        camera_.GetFarClipCoordinates(far_bottom_left_, far_top_right_);
+        min_ = volume_->GetViewBox().min_;
+        max_ = volume_->GetViewBox().max_;
+        difference_ = max_ - min_;
     }
 
     Camera camera_;
     std::shared_ptr<OrthographicViewVolume> volume_;
-    math::Vec3f near_bottom_left_;
-    math::Vec3f near_top_right_;
-    math::Vec3f far_bottom_left_;
-    math::Vec3f far_top_right_;
+    math::Vec3f min_;
+    math::Vec3f max_;
+    math::Vec3f difference_;
 };
 
 
 TEST_F(TestOrthographicViewVolume, IsCulled_Point_InVolume)
 {
-    EXPECT_FALSE(volume_->IsCulled(near_bottom_left_));
-    EXPECT_FALSE(volume_->IsCulled(near_top_right_));
+    EXPECT_FALSE(volume_->IsCulled(min_));
+    EXPECT_FALSE(volume_->IsCulled(max_));
     EXPECT_FALSE(volume_->IsCulled(volume_->GetViewBox().Center()));
-    EXPECT_FALSE(volume_->IsCulled(math::Vec3f(0.0F, 0.0F, (far_top_right_.z_ - near_top_right_.z_) * 0.5F)));
 }
 
 TEST_F(TestOrthographicViewVolume, IsCulled_Point_OutOfVolume)
 {
+    EXPECT_TRUE(volume_->IsCulled(min_ - difference_));
+    EXPECT_TRUE(volume_->IsCulled(max_ + difference_));
     EXPECT_TRUE(volume_->IsCulled(camera_.position_));
-    EXPECT_TRUE(volume_->IsCulled(far_bottom_left_));
-    EXPECT_TRUE(volume_->IsCulled(far_top_right_));
 }
 
 TEST_F(TestOrthographicViewVolume, IsCulled_Sphere_InVolume)
@@ -61,25 +59,23 @@ TEST_F(TestOrthographicViewVolume, IsCulled_Sphere_InVolume)
     sphere.center_ = volume_->GetViewBox().Center();
     EXPECT_FALSE(volume_->IsCulled(sphere));
 
-    sphere.center_ = near_bottom_left_;
+    sphere.center_ = min_;
     EXPECT_FALSE(volume_->IsCulled(sphere));
 
-    sphere.center_ = near_top_right_;
+    sphere.center_ = max_;
     EXPECT_FALSE(volume_->IsCulled(sphere));
 }
 
 TEST_F(TestOrthographicViewVolume, IsCulled_Sphere_OutOfVolume)
 {
     math::Sphere sphere(1.0F);
+    sphere.center_ = volume_->GetViewBox().Center() - difference_;
     EXPECT_TRUE(volume_->IsCulled(sphere));
 
-    sphere.center_ = camera_.position_;
+    sphere.center_ = min_ - difference_;
     EXPECT_TRUE(volume_->IsCulled(sphere));
 
-    sphere.center_ = near_bottom_left_ + math::Vec3f(0.0F, 0.0F, 1.0F);
-    EXPECT_TRUE(volume_->IsCulled(sphere));
-
-    sphere.center_ = near_top_right_ + math::Vec3f(0.0F, 0.0F, 1.0F);
+    sphere.center_ = max_ + difference_;
     EXPECT_TRUE(volume_->IsCulled(sphere));
 }
 
@@ -89,28 +85,28 @@ TEST_F(TestOrthographicViewVolume, IsCulled_Box_InVolume)
     EXPECT_FALSE(volume_->IsCulled(box));
 
     box.min_ = volume_->GetViewBox().Center();
-    box.max_ = math::Vec3f(1000.0F, 1000.0F, 1000.0F);
+    box.max_ = math::Vec3f(10000.0F, 10000.0F, 10000.0F);
     EXPECT_FALSE(volume_->IsCulled(box));
 
-    box.min_ = near_bottom_left_;
+    box.min_ = min_;
     EXPECT_FALSE(volume_->IsCulled(box));
 
-    box.min_ = near_top_right_;
+    box.min_ = max_;
     EXPECT_FALSE(volume_->IsCulled(box));
 }
 
 TEST_F(TestOrthographicViewVolume, IsCulled_Box_OutOfVolume)
 {
-    math::Box box = math::Box::Unit();
+    math::Box box{};
+    box.min_ = max_ + math::kEpsilon;
+    box.max_ = math::Vec3f(10000.0F, 10000.0F, 10000.0F);
     EXPECT_TRUE(volume_->IsCulled(box));
 
-    box.min_ = camera_.position_;
-    box.max_ = math::Vec3f(100.0F, 100.0F, 100.0F);
+    box.min_ = min_ - difference_;
+    box.max_ = min_ - math::kEpsilon;
     EXPECT_TRUE(volume_->IsCulled(box));
 
-    box.min_ = near_bottom_left_ + math::Vec3f(0.0F, 0.0F, 1.0F);
-    EXPECT_TRUE(volume_->IsCulled(box));
-
-    box.min_ = near_top_right_ + math::Vec3f(0.0F, 0.0F, 1.0F);
+    box.min_ = max_ + math::kEpsilon;
+    box.max_ = max_ + difference_;
     EXPECT_TRUE(volume_->IsCulled(box));
 }
